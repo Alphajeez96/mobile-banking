b2f024b8c6ceaafaeaa682e3f21586b6479eaa69
Prince <chimeremezechukwudire@gmail.com>
2020-08-15 21:18:48 +0100

    folder structure added

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+<page1></page1>
+  </div>
+</template>
+
+<script>
+import  page1 from '@/components/page1/page1.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    page1
+  }
+}
+</script>
