a8860bf07bcf15f75d5e50c4aaa5df22020a6c97
Prince <chimeremezechukwudire@gmail.com>
2020-08-16 15:50:27 +0100

    top nav added

--- src/components/partials/sidebar.vue
+++ src/components/partials/sidebar.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <!-- side nav here -->
-    <aside class="side__nav position-fixed overflow-auto ">
+    <aside class="side__nav position-fixed overflow-auto">
+    
       <!-- side logo here -->
       <span class="side__logo d-flex align-items-center px-3 py-4">
         <img src="../../assets/General/sidelogo.png" alt />
@@ -9,22 +10,50 @@
       <div class="border-b mb-4"></div>
 
       <!--first side nav-items here -->
-      <span class="side__nav__items d-flex align-items-center px-3 py-3" v-for="item in items1" :key="item.id">
-        <span class="ml-2 flex align-items-center ">
+      <span
+        class="side__nav__items d-flex align-items-center px-3 py-3"
+        v-for="item in items1"
+        :key="item.id"
+      >
+        <span class="ml-2 flex align-items-center">
           <img :src="item.icon" alt srcset />
-             <!-- <span>Home</span> -->
+          <!-- <span>Home</span> -->
         </span>
       </span>
       <div class="border-b mb-4"></div>
 
-       <!--Second side nav-items here -->
-      <span class="side__nav__items d-flex align-items-center px-3 py-3" v-for="item in items2" :key="item.id">
-        <span class="ml-2 flex align-items-center ">
+      <!--Second side nav-items here -->
+      <span
+        class="side__nav__items d-flex align-items-center px-3 py-3"
+        v-for="item in items2"
+        :key="item.id"
+      >
+        <span class="ml-2 flex align-items-center">
           <img :src="item.icon" alt srcset />
-             <!-- <span>Home</span> -->
+          <!-- <span>Home</span> -->
         </span>
       </span>
-      <div class="border-b"></div>
+      <div class="border-b mb-4"></div>
+
+      <!--Logout nav-items here -->
+      <span class="side__nav__items logout d-flex align-items-center px-3 py-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+        >
+          <rect opacity="0.2" width="40" height="40" rx="20" fill="#E8E7EA" />
+          <path
+            d="M18 25L23 20L18 15"
+            stroke="#23B3E8"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </span>
     </aside>
   </div>
 </template>
@@ -62,36 +91,36 @@ export default {
           route: "/folder",
         },
       ],
- 
+
       items2: [
         {
           text: "PN",
           icon:
-           "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%2323B3E8'/%3E%3Cpath d='M4.456 5.77H6.304C6.756 5.77 7.108 5.884 7.36 6.112C7.612 6.336 7.738 6.652 7.738 7.06C7.738 7.468 7.612 7.786 7.36 8.014C7.108 8.242 6.756 8.356 6.304 8.356H5.23V10H4.456V5.77ZM6.208 7.762C6.74 7.762 7.006 7.53 7.006 7.066C7.006 6.83 6.94 6.656 6.808 6.544C6.676 6.428 6.476 6.37 6.208 6.37H5.23V7.762H6.208ZM11.9869 5.77V10H11.4049L9.13694 7.048V10H8.41694V5.77H8.99294L11.2609 8.716V5.77H11.9869Z' fill='white'/%3E%3C/svg%3E",
+            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%2323B3E8'/%3E%3Cpath d='M4.456 5.77H6.304C6.756 5.77 7.108 5.884 7.36 6.112C7.612 6.336 7.738 6.652 7.738 7.06C7.738 7.468 7.612 7.786 7.36 8.014C7.108 8.242 6.756 8.356 6.304 8.356H5.23V10H4.456V5.77ZM6.208 7.762C6.74 7.762 7.006 7.53 7.006 7.066C7.006 6.83 6.94 6.656 6.808 6.544C6.676 6.428 6.476 6.37 6.208 6.37H5.23V7.762H6.208ZM11.9869 5.77V10H11.4049L9.13694 7.048V10H8.41694V5.77H8.99294L11.2609 8.716V5.77H11.9869Z' fill='white'/%3E%3C/svg%3E",
           route: "/PN",
         },
         {
           text: "JI",
           icon:
-           "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Cpath d='M5.964 9.43L6.384 9.4C6.74 9.376 6.918 9.18 6.918 8.812V5.77H7.692V8.806C7.692 9.182 7.592 9.474 7.392 9.682C7.196 9.886 6.902 10 6.51 10.024L6.018 10.054L5.964 9.43ZM8.60639 5.77H9.38039V10H8.60639V5.77Z' fill='white'/%3E%3C/svg%3E",
+            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Crect width='16' height='16' rx='8' fill='%234DBD98'/%3E%3Cpath d='M5.964 9.43L6.384 9.4C6.74 9.376 6.918 9.18 6.918 8.812V5.77H7.692V8.806C7.692 9.182 7.592 9.474 7.392 9.682C7.196 9.886 6.902 10 6.51 10.024L6.018 10.054L5.964 9.43ZM8.60639 5.77H9.38039V10H8.60639V5.77Z' fill='white'/%3E%3C/svg%3E",
           route: "/JI",
         },
         {
           text: "LG",
           icon:
-           "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%233D43A2'/%3E%3Cpath d='M4.456 5.77H5.23V9.364H7.204V10H4.456V5.77ZM11.0836 7.714V9.742C10.8916 9.838 10.6576 9.914 10.3816 9.97C10.1096 10.026 9.83161 10.054 9.54761 10.054C9.11161 10.054 8.73361 9.968 8.41361 9.796C8.09361 9.62 7.84761 9.37 7.67561 9.046C7.50761 8.718 7.42361 8.33 7.42361 7.882C7.42361 7.438 7.50761 7.054 7.67561 6.73C7.84761 6.402 8.08961 6.152 8.40161 5.98C8.71761 5.804 9.08761 5.716 9.51161 5.716C9.80761 5.716 10.0856 5.76 10.3456 5.848C10.6056 5.932 10.8216 6.054 10.9936 6.214L10.7356 6.772C10.5396 6.62 10.3456 6.512 10.1536 6.448C9.96561 6.38 9.75761 6.346 9.52961 6.346C9.09361 6.346 8.76361 6.476 8.53961 6.736C8.31961 6.992 8.20961 7.374 8.20961 7.882C8.20961 8.922 8.66561 9.442 9.57761 9.442C9.84961 9.442 10.1216 9.404 10.3936 9.328V8.266H9.48161V7.714H11.0836Z' fill='white'/%3E%3C/svg%3E",
+            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%233D43A2'/%3E%3Cpath d='M4.456 5.77H5.23V9.364H7.204V10H4.456V5.77ZM11.0836 7.714V9.742C10.8916 9.838 10.6576 9.914 10.3816 9.97C10.1096 10.026 9.83161 10.054 9.54761 10.054C9.11161 10.054 8.73361 9.968 8.41361 9.796C8.09361 9.62 7.84761 9.37 7.67561 9.046C7.50761 8.718 7.42361 8.33 7.42361 7.882C7.42361 7.438 7.50761 7.054 7.67561 6.73C7.84761 6.402 8.08961 6.152 8.40161 5.98C8.71761 5.804 9.08761 5.716 9.51161 5.716C9.80761 5.716 10.0856 5.76 10.3456 5.848C10.6056 5.932 10.8216 6.054 10.9936 6.214L10.7356 6.772C10.5396 6.62 10.3456 6.512 10.1536 6.448C9.96561 6.38 9.75761 6.346 9.52961 6.346C9.09361 6.346 8.76361 6.476 8.53961 6.736C8.31961 6.992 8.20961 7.374 8.20961 7.882C8.20961 8.922 8.66561 9.442 9.57761 9.442C9.84961 9.442 10.1216 9.404 10.3936 9.328V8.266H9.48161V7.714H11.0836Z' fill='white'/%3E%3C/svg%3E",
           route: "/LG",
         },
         {
           text: "AW",
           icon:
-          "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%23F15832'/%3E%3Cpath d='M6.264 9.01H4.206L3.78 10H3L4.914 5.77H5.544L7.458 10H6.69L6.264 9.01ZM6.006 8.41L5.232 6.61L4.458 8.41H6.006ZM13.5648 5.77L12.0708 10H11.4408L10.3788 6.958L9.31083 10H8.68083L7.19283 5.77H7.97283L9.02283 8.92L10.1268 5.77H10.6728L11.7468 8.938L12.8268 5.77H13.5648Z' fill='white'/%3E%3C/svg%3E",
+            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%23F15832'/%3E%3Cpath d='M6.264 9.01H4.206L3.78 10H3L4.914 5.77H5.544L7.458 10H6.69L6.264 9.01ZM6.006 8.41L5.232 6.61L4.458 8.41H6.006ZM13.5648 5.77L12.0708 10H11.4408L10.3788 6.958L9.31083 10H8.68083L7.19283 5.77H7.97283L9.02283 8.92L10.1268 5.77H10.6728L11.7468 8.938L12.8268 5.77H13.5648Z' fill='white'/%3E%3C/svg%3E",
           route: "/AW1",
         },
-          {
+        {
           text: "AW",
           icon:
-          "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%23004F71'/%3E%3Cpath d='M6.264 9.01H4.206L3.78 10H3L4.914 5.77H5.544L7.458 10H6.69L6.264 9.01ZM6.006 8.41L5.232 6.61L4.458 8.41H6.006ZM13.5648 5.77L12.0708 10H11.4408L10.3788 6.958L9.31083 10H8.68083L7.19283 5.77H7.97283L9.02283 8.92L10.1268 5.77H10.6728L11.7468 8.938L12.8268 5.77H13.5648Z' fill='white'/%3E%3C/svg%3E",
+            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'%3E%3Crect width='16' height='16' rx='8' fill='%23004F71'/%3E%3Cpath d='M6.264 9.01H4.206L3.78 10H3L4.914 5.77H5.544L7.458 10H6.69L6.264 9.01ZM6.006 8.41L5.232 6.61L4.458 8.41H6.006ZM13.5648 5.77L12.0708 10H11.4408L10.3788 6.958L9.31083 10H8.68083L7.19283 5.77H7.97283L9.02283 8.92L10.1268 5.77H10.6728L11.7468 8.938L12.8268 5.77H13.5648Z' fill='white'/%3E%3C/svg%3E",
           route: "/AW2",
         },
       ],
@@ -102,14 +131,13 @@ export default {
 
 <style scoped>
 .side__nav {
- 
   height: 100%;
   background: #052231;
-  width: 80px;
+  width: 95px;
 }
 
 .side__logo {
-width:100%
+  width: 100%;
 }
 
 .border-b {
@@ -119,5 +147,8 @@ width:100%
 
 .side__nav__items {
   cursor: pointer;
+}
+.logout {
+  margin-top: 100px;
 }
 </style>
