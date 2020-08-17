2e68a14870fdfd05b7dd54e4a9c4b6411a99d6d9
Prince <chimeremezechukwudire@gmail.com>
2020-08-17 15:48:31 +0100

    test case card added

---
+++ src/components/Layout/testcase.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- Todo Header Column here -->
+    <div >
+      <div class="text-left header">
+        <div>{{title}}</div> 
+      </div>
+
+       <div class="Testcase__cards mt-3">
+       
+       </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+<style scoped>
+.header {
+  color: #03293d;
+  font-size: var(--main-font-size);
+  font-weight: Bold;
+}
+.Testcase__cards{
+     background: white;
+  height: 120vh;
+}
+</style>>
--- src/components/page2/page2.vue
+++ src/components/page2/page2.vue
@@ -7,39 +7,41 @@
       <div class="row row-cols-5 main ml-5 pl-1">
 
       <div class="col-md-3">
+
          <!-- Backlog here -->
         <backlog title="Modules"></backlog>
       </div>
 
-      <div class="col-md-4">
-      qqq
+      <div class="col-md-5">
+    <testcases title="Added Test Case"></testcases>
       </div>
 
-         <div class="col-md-4">
+         <div class="col-md-5">
       sss
       </div>
      
      
         <!-- Todo Header Column here -->
-        
-        
+           
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import mnav from '../partials/navbar'
+
 import topnav from "../partials/topnav2";
 import backlog from "../Layout/backlog";
 import todo from "../Layout/todos";
+import testcases from '../Layout/testcase'
 export default {
   name: "page1",
   components: {
-      mnav,
+      
     topnav,
     backlog,
     todo,
+    testcases,
   },
 };
 </script>
