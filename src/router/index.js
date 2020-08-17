7851758cae750b186ea19f4705f65a65f6a55ed6
Prince <chimeremezechukwudire@gmail.com>
2020-08-18 00:35:48 +0100

    build

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from "../views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [{
-        path: "/returns",
+        path: "/",
         name: "Home",
         component: Home,
     },
